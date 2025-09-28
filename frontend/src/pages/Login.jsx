85f3dfaa3605ff2844376f6ad8b9836e1afa92b2
mohammedjunaidali6 <60916872+mohammedjunaidali6@users.noreply.github.com>
2025-09-28 20:14:18 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -28,7 +28,7 @@ function Login() {
             return handleError("email and password are required");
         }
         try{
-            const url = `https://mern-app-api-gamma.vercel.app/auth/login`;
+            const url = "https://mern-app-api-gamma.vercel.app/auth/login";
             const response = await fetch(url, {
                 method: "POST",
                 headers:{
